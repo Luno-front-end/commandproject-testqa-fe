0d99178ac4f92fc4ad3a8dbfd3901ade6fe6f45d
TOR968 <tor968@yahoo.com>
2021-04-04 21:14:45 +0300

    Create PrivateRoute.jsx

---
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+import { isAuthenticated } from '../redux/auth/auth-selectors';
+import routes from '../routes';
+
+const PrivateRoute = ({ component: Component, ...routeProps }) => {
+  const isAuthenticatedUser = useSelector(isAuthenticated);
+  return (
+    <Route
+      {...routeProps}
+      render={props =>
+        isAuthenticatedUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={routes.home} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
