d3ef04840772b0ce45b598bcc04eee29747d57c4
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-04-04 11:56:02 +0300

    add modern normalize

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'modern-normalize/modern-normalize.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-
