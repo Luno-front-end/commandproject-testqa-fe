ec40041e0bb253067d6e6970a9879f55185586d1
TOR968 <tor968@yahoo.com>
2021-04-04 21:13:15 +0300

    Create UserMenu.jsx

---
+++ src/UserMenu/UserMenu.jsx
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useCallback } from 'react';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Button from '@material-ui/core/Button';
+import { name, loading } from '../redux/auth/auth-selectors';
+import { logoutUser } from '../redux/auth/auth-operations';
+
+const UserMenu = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(name);
+  const loadingAuth = useSelector(loading);
+
+  const logout = useCallback(() => dispatch(logoutUser()), [dispatch]);
+
+  return (
+    <div className="container-user">
+      <div className="user-greeting">Welcome {userName}!</div>
+      {loadingAuth ? (
+        'Loading...'
+      ) : (
+        <Button variant="outlined" color="action" onClick={logout}>
+          <ExitToAppIcon />
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
