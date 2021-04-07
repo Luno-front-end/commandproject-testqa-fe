82423a83ae94d6645eb0eb26fcf29c6f1e1f851e
TOR968 <tor968@yahoo.com>
2021-04-08 00:02:04 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,54 +1,41 @@
-// import { configureStore } from '@reduxjs/toolkit';
-
-// import testReducer from './testSlice';
-
-///////////////////////////////////////////////////
-// export const store = configureStore({
-//   reducer: { allTests: testReducer },
-//   devTools: process.env.NODE_ENV !== 'production',
-// });
-// ///////////////////////////////////////////////
-
-// import { configureStore } from '@reduxjs/toolkit'
-// const reducer = {
-//   todos: todosReducer,
-//   visibility: visibilityReducer,
-// }
-// const store = configureStore({
-//   reducer,
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-//   devTools: process.env.NODE_ENV !== 'production',
-//   preloadedState,
-//   enhancers: [reduxBatch],
-// })
-
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import storage from 'redux-persist/lib/storage';
-import { persistStore, persistReducer } from 'redux-persist';
-
-import authUsersReducer from './auth/auth-reducer';
+import { configureStore } from '@reduxjs/toolkit';
 
 import testReducer from './testSlice';
 
-const authPersistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
-};
-
-const rootReducer = combineReducers({
-  auth: persistReducer(authPersistConfig, authUsersReducer),
+export const store = configureStore({
   reducer: { allTests: testReducer },
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
+// ///////////////////////////////////////////////
 
-export const persistor = persistStore(store);
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+// import storage from 'redux-persist/lib/storage';
+// import { persistStore, persistReducer } from 'redux-persist';
 
-export default store;
+// import authUsersReducer from './auth/auth-reducer';
+
+// import testReducer from './testSlice';
+
+// const authPersistConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
+
+// const rootReducer = combineReducers({
+//   auth: persistReducer(authPersistConfig, authUsersReducer),
+//   reducer: { allTests: testReducer },
+//   devTools: process.env.NODE_ENV !== 'production',
+// });
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk)),
+// );
+
+// export const persistor = persistStore(store);
+
+// export default store;
