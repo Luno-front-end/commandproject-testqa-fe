b23d4eaa4393d6cddfbbc599300c74b8eb73b054
TOR968 <tor968@yahoo.com>
2021-04-08 00:01:52 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,18 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useCallback } from 'react';
-import { name, loading } from '../../redux/auth/auth-selectors';
-import { logoutUser } from '../../redux/auth/auth-operations';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useCallback } from 'react';
+// import { name, loading } from '../../redux/auth/auth-selectors';
+// import { logoutUser } from '../../redux/auth/auth-operations';
 import sprite from '../../images/sprite.svg'
 
 const UserMenu = () => {
-  const dispatch = useDispatch();
-  const userName = useSelector(name);
-  const loadingAuth = useSelector(loading);
+  // const dispatch = useDispatch();
+  // const userName = useSelector(name);
+  // const loadingAuth = useSelector(loading);
 
-  const logout = useCallback(() => dispatch(logoutUser()), [dispatch]);
+  // const logout = useCallback(() => dispatch(logoutUser()), [dispatch]);
+
+  const logout = true
+  const loadingAuth = false
 
   return (
     <div className="container-user">
@@ -17,6 +20,16 @@ const UserMenu = () => {
         {/* {userName.match(/[A-Z]/g).join('')}*/}</div>
         {/* {userName} */}
       <span className="userName">Dmitri</span>
+      {/* {loadingAuth ? (
+        '...'
+      ) : (
+          <div className="sing-out">
+            <svg width="16" height="16" onClick={logout}>
+              signOut
+                 <use href={sprite + '#signOut'}></use>
+            </svg>
+          </div>
+      )} */}
       {loadingAuth ? (
         '...'
       ) : (
