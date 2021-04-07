9ef80daa62cc56fb6fe5341344400bcbecc72c01
aleks-goit <60502821+aleks-goit@users.noreply.github.com>
2021-04-07 21:19:22 +0300

    auth layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import MainPage from './components/MainPage/MainPage.jsx';
 import TestPage from './components/TestPage/TestPage.jsx';
 import AppBar from './components/AppBar';
+import AuthPage from './components/AuthPage/AuthPage';
 
 // import TestSpriteSVG from './components/TestSpriteSVG.jsx';
 
 function App() {
   return (
     <>
-      <AppBar />
-      <MainPage />
+      {/* <AppBar />
+      <MainPage /> */}
+      <AuthPage />
       {/* <TestPage /> */}
       {/* <TestSpriteSVG /> */}
     </>
