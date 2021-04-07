3b5e730d9bb83852c66a14539437c75b1ba96201
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-04-07 21:02:08 +0300

    add fetch question

--- src/components/TestPage/TestPage.jsx
+++ src/components/TestPage/TestPage.jsx
@@ -1,45 +1,86 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, Link, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import { getAllTest } from '../../redux/tetstOperaion';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
 import sprite from '../../images/sprite.svg';
 
 function Test() {
   const location = useLocation();
-  const query = new URLSearchParams(location.search).get('name');
+  const history = useHistory();
   const dispatch = useDispatch();
+  const allTests = useSelector(({ allTestsR }) => allTestsR);
+
+  const query = new URLSearchParams(location.search).get('name');
+
+  const [indexQuestion, setindexQuestion] = useState(0);
+
+  useEffect(() => {
+    if (!query || (query !== 'qa' && query !== 'testTheory')) {
+      history.push(`/`);
+    }
+  }, [history, query]);
 
   useEffect(() => {
     dispatch(getAllTest(query));
   }, [dispatch]);
 
+  function nextQuestion() {
+    if (indexQuestion >= 11) {
+      redirectResultsPage();
+    }
+    setindexQuestion(prevState => prevState + 1);
+  }
+
+  function previousQuestion() {
+    if (indexQuestion <= 0) {
+      return;
+    }
+    setindexQuestion(prevState => prevState - 1);
+  }
+
+  function redirectResultsPage() {
+    history.push(`/results?name=${query}`);
+  }
+
   return (
     <div className="container bgColorTest">
       <div className="flexContainer">
-        <p className="nameOfTest">[ Testing theory_]</p>
-        <button className="btnThirdTest">Finish test</button>
+        <p className="nameOfTest">
+          {query === 'qa' ? '[ QA technical training_]' : '[ Testing theory_]'}
+        </p>
+        <Link to={`/`} className="btnThirdTest">
+          Finish test
+        </Link>
       </div>
       <form className="formOfQuestionTest">
-        <p>Question 3 / 12 </p>
-        <p>What is regression testing?</p>
+        <p>{`Question ${indexQuestion + 1} / 12 `}</p>
+        <p>{allTests[indexQuestion]?.question}</p>
 
-        {/* {questions && questions.map(question => (<ul key={question.id}>
-          <li>
-            question.SOMETEXT
-          </li>
-        </ul>))} */}
+        <ul>
+          {allTests[indexQuestion]?.answers.map(arrAnswers => (
+            <li key={Math.random()}>
+              <input type="checkbox" />
+              <p>{arrAnswers}</p>
+            </li>
+          ))}
+        </ul>
       </form>
       <div className="btnsBlockTest">
-        <button className="btnPrimaryTest">
+        <button
+          className="btnPrimaryTest"
+          onClick={previousQuestion}
+          disabled={indexQuestion <= 0 ? true : false}
+        >
           <svg className="markerPrimaryTest">
             <use href={sprite + '#arrowLeft'}></use>
           </svg>
           <p className="textPrimaryBtnTest">Previous question</p>
         </button>
-        <button className="btnSecondaryTest">
-          <p className="textSecondaryBtnTest">Next question</p>
+        <button className="btnSecondaryTest" onClick={nextQuestion}>
+          <p className="textSecondaryBtnTest">
+            {indexQuestion >= 11 ? 'finish' : 'Next question'}
+          </p>
           <svg className="markerSecondaryTest">
             <use href={sprite + '#arrowLeft'}></use>
           </svg>
