c0e5f4c93abf0d7cb8156fd218718098e7637de2
TOR968 <tor968@yahoo.com>
2021-04-08 00:01:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// // import { PersistGate } from 'redux-persist/integration/react';
+// import 'modern-normalize/modern-normalize.css';
+// import './index.scss';
+
+// // import store, { persistor } from './redux/store';
+// import { store } from './redux/store';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       {/* <PersistGate
+//         loading={null}
+//         persistor={persistor}
+//       > */}
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//       {/* </PersistGate> */}
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import 'modern-normalize/modern-normalize.css';
 import './index.scss';
-
-import store, { persistor } from './redux/store';
+import { store } from './redux/store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
