8f7524caca19713b5c07b0bb862d002a00df9ed8
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2021-04-08 20:23:53 +0300

    markup done

--- src/components/proTestUsefulInfo/proTestUsefulInfo.jsx
+++ src/components/proTestUsefulInfo/proTestUsefulInfo.jsx
@@ -1,7 +1,24 @@
-import sprite from '../../images/footer/sprite.svg';
+
 
 export default function proTestUsefulInfo() {
   return (
-    <div></div>
+    <section className="container proTestUsefulInfo">
+      <h2 className="listNameH">Useful <br />literature</h2>
+      <ol className="sectionList">
+        <li>Testing dot.com Savin.</li>
+        <li>
+          A mental hospital in the hands of <br />
+          patients.
+        </li>
+        <li>Scrum. J. Sutherland.</li>
+      </ol>
+      <h2 className="listNameH vectorH">Useful <br />resources</h2>
+      <ol className="sectionList">
+        <li><a href="https://dou.ua/" className="linkOl">dou.ua</a></li>
+        <li><a href="https://habr.com/ru/" className="linkOl">Habr</a></li>
+        <li><a href="https://www.facebook.com/" className="linkOl">facebook.com/QA</a></li>
+        <li><a href="https://goit.ua/" className="linkOl">goit.ua</a></li>
+      </ol>
+    </section>
   );
 }
