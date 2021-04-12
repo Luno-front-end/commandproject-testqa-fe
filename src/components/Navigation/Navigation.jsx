f5713ff03a7db13ce215d0c134cb62eaf4295928
TOR968 <tor968@yahoo.com>
2021-04-12 21:49:15 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -30,32 +30,34 @@ const Navigation = () => {
         </NavLink>
         <div className="nav-and-burger">
           <div className="nav-menu">
-            {isLoggedIn && (
-              <>
-                <NavLink
-                  exact
-                  to={routes.homePage}
-                  className="navLink"
-                  activeClassName="navLink--active"
-                >
-                  Home
-                </NavLink>
-                <NavLink
-                  to={routes.materialsPage}
-                  className="navLink"
-                  activeClassName="navLink--active"
-                >
-                  Materials
-                </NavLink>
-              </>
-            )}
-            <NavLink
-              to={routes.contactsPage}
-              className="navLink"
-              activeClassName="navLink--active"
-            >
-              Contacts
-            </NavLink>
+            <div className="nav-menu-link">
+              {isLoggedIn && (
+                <>
+                  <NavLink
+                    exact
+                    to={routes.homePage}
+                    className="navLink"
+                    activeClassName="navLink--active"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to={routes.materialsPage}
+                    className="navLink"
+                    activeClassName="navLink--active"
+                  >
+                    Materials
+                  </NavLink>
+                </>
+              )}
+              <NavLink
+                to={routes.contactsPage}
+                className="navLink"
+                activeClassName="navLink--active"
+              >
+                Contacts
+              </NavLink>
+            </div>
 
             {isLoggedIn && <UserMenu />}
           </div>
