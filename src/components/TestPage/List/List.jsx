719919531b1f8dea995c897bab551062d736b147
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-04-13 23:30:20 +0300

    fix check box

--- src/components/TestPage/List/List.jsx
+++ src/components/TestPage/List/List.jsx
@@ -3,19 +3,19 @@ export default function List({ el, id, onChange, answer }) {
     <li className="flexInputAndTextTest">
       <div className="activeCircleTestList">
         <input
-        type="radio"
-        name="answer"
-        className="inputBtn"
-        id={id}
-        value={el}
-        onChange={e => onChange(e)}
-        checked={answer === el}
-      />
-</div>
+          type="radio"
+          name="answer"
+          className="inputBtn"
+          id={id}
+          value={el}
+          onChange={e => onChange(e)}
+          checked={answer === el}
+        />
+      </div>
       {/* <div className="activeCheckTestList"></div> */}
 
       <label className="textOfAnswersTest" htmlFor={id}>
-        <p> {el}</p>
+        <p className="gapInputAndAnswer"> {el}</p>
       </label>
     </li>
   );
